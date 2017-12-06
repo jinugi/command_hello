81644d2be19a4c13d1b237e6f39e8d44f0129844
jinugi <jinugi94@gmail.com>
2017-12-06 11:49:14 +0900

    remove git comments

--- hello.py
+++ hello.py
@@ -1,7 +1,3 @@
-// ++++++++++++++++++++++++++++
-// For command line git tutorial 
-// GitHub modification 
-// ++++++++++++++++++++++++++++
 print("hello world")
 print("Hi jinuk")
 print("Hi Wonji")
